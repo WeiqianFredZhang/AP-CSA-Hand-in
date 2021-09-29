b0576e30bed3c0c68c8dc349fd3eab16a98f3431
Technoblade (CS Edition) <freddyzhang666@gmail.com>
2021-09-29 09:23:40 -0700

    Update Main.java

--- Sept 29th/Main.java
+++ Sept 29th/Main.java
@@ -1,1 +1,12 @@
+/*
+    Problem Set 2-A Solution
+    Fred Zhang
+    Sept 20th
+ */
 
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.display();
+    }
+}
